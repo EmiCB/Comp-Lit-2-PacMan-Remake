a508b138e32487516baeaddd43b2ba643324a012
Emi Brown <EmiCB@users.noreply.github.com>
2016-09-15 09:03:32 -0500

    Version 0.1
    Added:
    - Basic Movement
    - Turns corresponding to arrow keys
    - Pac-Man sprite
    
    Bugs:
    - Movement is weird due to turning

---
+++ Pac-Man Remake/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    //Allows you to change varriables using the Unity inspector
+    public float speed = 6.0F;
+    public float gravity = 20.0F;
+
+    //Movement using the Player Controller component in Unity
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            //Locks direction the player is facing so that it matches the direction the player is moving
+
+            //FIX THE CODE FOR THIS
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                transform.forward = new Vector3(1f, 0f, 0f);
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+                transform.forward = new Vector3(-1f, 0f, 0f);
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                transform.forward = new Vector3(0f, 0f, 1f);
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                transform.forward = new Vector3(0f, 0f, -1f);
+            //END
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
