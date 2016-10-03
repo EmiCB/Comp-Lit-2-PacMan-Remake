f3b108941950f2a730845baf179d80509c089687
Emi Brown <EmiCB@users.noreply.github.com>
2016-10-03 11:39:56 -0500

    Update 0.2
    Added:
    - Pellet, Power Pellet, Cherry, Strawberry, and Orange score counts and tags

---
+++ Pac-Man Remake/Assets/Scripts/BasicCollectable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// NOTES:
+/// Small collectables give 10 points
+/// Big collectables give 50 points
+
+public class BasicCollectable : MonoBehaviour
+{
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+	    
+	}
+}
--- Pac-Man Remake/Assets/Scripts/PlayerController.cs
+++ Pac-Man Remake/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /// + = Add
 /// - = Remove
@@ -6,12 +7,27 @@
 /// NOTES:
 /// + continuous movement
 /// + animations
+/// + function for displaying score
 
 public class PlayerController : MonoBehaviour
 {
     //Allows you to change varriables using the Unity inspector
     public float speed = 6.0F;
     public float gravity = 20.0F;
+
+    //Sets Text scoreText
+    public Text scoreText;
+
+    //Adds a score integer
+    private int score;
+
+    void Start()
+    {
+        //Sets the score to zero
+        score = 0;
+        //Displays your current score
+        scoreText.text = "Score: " + score.ToString();
+    }
 
     //Movement using the Player Controller component in Unity
     private Vector3 moveDirection = Vector3.zero;
@@ -37,4 +53,71 @@ public class PlayerController : MonoBehaviour
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
+
+    //Pickups
+    void OnTriggerEnter(Collider other)
+    {
+        // Picks up certain game objects witth the "Pellet" tag
+        if (other.gameObject.CompareTag("Pellet"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Adds 10 points to your score
+            score = score + 10;
+
+            //Displays your current score
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+        // Picks up certain game objects witth the "PowerPellet" tag
+        if (other.gameObject.CompareTag("PowerPellet"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Adds 50 points to your score
+            score = score + 50;
+
+            //Displays your current score
+            scoreText.text = "Score: " + score.ToString();
+
+            //ADD CODE TO ALLOW PLAYER TO EAT GHOSTS
+        }
+
+        // Picks up certain game objects witth the "Cherry" tag
+        if (other.gameObject.CompareTag("Cherry"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Adds 100 points to your score
+            score = score + 100;
+
+            //Displays your current score
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+        // Picks up certain game objects witth the "Strawberry" tag
+        if (other.gameObject.CompareTag("Strawberry"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Adds 300 points to your score
+            score = score + 300;
+
+            //Displays your current score
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+        // Picks up certain game objects witth the "Orange" tag
+        if (other.gameObject.CompareTag("Orange"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Adds 500 points to your score
+            score = score + 500;
+
+            //Displays your current score
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
 }
