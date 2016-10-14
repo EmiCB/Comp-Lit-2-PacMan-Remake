652be3a4e0d47003d1964880b2a2cd878744e76a
Emi Brown <EmiCB@users.noreply.github.com>
2016-10-14 10:41:44 -0500

    Update 0.1.3
    Added:
    - Added scores for Apple, Pineapple, Galaxian Spaceship, Bell, and Key.
    
    Working on:
    - Ability to eat ghosts for 10 seconds when power pellet is eaten

--- Pac-Man Remake/Assets/Scripts/PlayerController.cs
+++ Pac-Man Remake/Assets/Scripts/PlayerController.cs
@@ -7,25 +7,27 @@ using UnityEngine.UI;
 /// NOTES:
 /// + continuous movement
 /// + animations
-/// + function for displaying score
 
 public class PlayerController : MonoBehaviour
 {
-    //Allows you to change varriables using the Unity inspector
+    // Allows you to change varriables using the Unity inspector
     public float speed = 6.0F;
     public float gravity = 20.0F;
 
-    //Sets Text scoreText
+	// Time that power pellet is effective
+	public float eatTime = 10.0f;
+
+    // Sets the Text scoreText
     public Text scoreText;
 
-    //Adds a score integer
+    // Adds a score integer
     private int score;
 
     void Start()
     {
-        //Sets the score to zero
+        // Sets the score to zero
         score = 0;
-        //Displays your current score
+        // Displays your current score
         scoreText.text = "Score: " + score.ToString();
     }
 
@@ -54,70 +56,131 @@ public class PlayerController : MonoBehaviour
         controller.Move(moveDirection * Time.deltaTime);
     }
 
-    //Pickups
+    // Pickups
     void OnTriggerEnter(Collider other)
     {
-        // Picks up certain game objects witth the "Pellet" tag
+        // Picks up certain game objects with the "Pellet" tag
         if (other.gameObject.CompareTag("Pellet"))
         {
             other.gameObject.SetActive(false);
 
-            //Adds 10 points to your score
+            // Adds 10 points to your score
             score = score + 10;
 
-            //Displays your current score
+            // Displays your current score
             scoreText.text = "Score: " + score.ToString();
         }
 
-        // Picks up certain game objects witth the "PowerPellet" tag
+        // Picks up certain game objects with the "PowerPellet" tag
         if (other.gameObject.CompareTag("PowerPellet"))
         {
             other.gameObject.SetActive(false);
 
-            //Adds 50 points to your score
+            // Adds 50 points to your score
             score = score + 50;
 
             //Displays your current score
             scoreText.text = "Score: " + score.ToString();
 
-            //ADD CODE TO ALLOW PLAYER TO EAT GHOSTS
+            //FINISH CODE TO ALLOW PLAYER TO EAT GHOSTS FOR A CERTAIN AMOUNT OF TIME
+
         }
 
-        // Picks up certain game objects witth the "Cherry" tag
+        // Picks up certain game objects with the "Cherry" tag
         if (other.gameObject.CompareTag("Cherry"))
         {
             other.gameObject.SetActive(false);
 
-            //Adds 100 points to your score
+            // Adds 100 points to your score
             score = score + 100;
 
-            //Displays your current score
+            // Displays your current score
             scoreText.text = "Score: " + score.ToString();
         }
 
-        // Picks up certain game objects witth the "Strawberry" tag
+        // Picks up certain game objects with the "Strawberry" tag
         if (other.gameObject.CompareTag("Strawberry"))
         {
             other.gameObject.SetActive(false);
 
-            //Adds 300 points to your score
+            // Adds 300 points to your score
             score = score + 300;
 
-            //Displays your current score
+            // Displays your current score
             scoreText.text = "Score: " + score.ToString();
         }
 
-        // Picks up certain game objects witth the "Orange" tag
+        // Picks up certain game objects with the "Orange" tag
         if (other.gameObject.CompareTag("Orange"))
         {
             other.gameObject.SetActive(false);
 
-            //Adds 500 points to your score
+            // Adds 500 points to your score
             score = score + 500;
 
-            //Displays your current score
+            // Displays your current score
             scoreText.text = "Score: " + score.ToString();
         }
+
+		// Picks up certain game objects with the "Apple" tag
+		if (other.gameObject.CompareTag("Apple"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Adds 700 points to your score
+			score = score + 700;
+
+			// Displays your current score
+			scoreText.text = "Score: " + score.ToString();
+		}
+
+		// Picks up certain game objects with the "Pineapple" tag
+		if (other.gameObject.CompareTag("Pineapple"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Adds 1000 points to your score
+			score = score + 1000;
+
+			// Displays your current score
+			scoreText.text = "Score: " + score.ToString();
+		}
+
+		// Picks up certain game objects with the "Galaxian Spaceship" tag
+		if (other.gameObject.CompareTag("Galaxian Spaceship"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Adds 2000 points to your score
+			score = score + 2000;
+
+			// Displays your current score
+			scoreText.text = "Score: " + score.ToString();
+		}
+
+		// Picks up certain game objects with the "Bell" tag
+		if (other.gameObject.CompareTag("Bell"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Adds 3000 points to your score
+			score = score + 3000;
+
+			// Displays your current score
+			scoreText.text = "Score: " + score.ToString();
+		}
+
+		// Picks up certain game objects with the "Key" tag
+		if (other.gameObject.CompareTag("Key"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Adds 5000 points to your score
+			score = score + 5000;
+
+			// Displays your current score
+			scoreText.text = "Score: " + score.ToString();
+		}
     }
 
 }
--- Pac-Man Remake/Assets/Scripts/_GameController.cs
+++ Pac-Man Remake/Assets/Scripts/_GameController.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// + = Add
+/// - = Remove
+/// 
+/// NOTES:
+/// + code to spawn objects at random points in each map
+/// + ghost house code
+
 public class _GameController : MonoBehaviour 
 {
 	
